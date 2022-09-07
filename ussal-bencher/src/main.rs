8d129c7f48bc571a1374eb630167226ad0f5bba2
Lucas Kent <rubickent@gmail.com>
2022-09-07 23:19:43 +1000

    Implement some writing logic

--- ussal-bencher/src/main.rs
+++ ussal-bencher/src/main.rs
@@ -5,8 +5,18 @@ use ussal_shared::{BenchMeasurement, BenchResult, BenchRun};
 
 /// `cargo bench`
 /// flags:
-/// --ci        - writes new file to `bench_ci_history/benchn.cbor`, combine all of `bench_logs/` into `bench_ci_history_combined.cbor`
-/// --file-name - write output to specified filename
+/// --ci
+///     1. CI infrastructure runs: `git fetch origin/gh-pages; git checkout origin/gh-pages -- bench_ci_web_root`
+///     2. CI infrastructure runs: `cargo bench --ci` which will:
+///         1. writes new file to `bench_ci_web_root/history/benchn.cbor` # TODO: how do I persist this folder?
+///         2. combine all of `bench_ci_web_root/history/` into `bench_ci_web_root/benches.cbor`
+///         3. Generates viewer html + wasm into `bench_ci_web_root/`
+///     3. CI infrastructure then needs to:
+///        on main branch checkin: copy bench_ci_web_root to gh-pages/bench_ci_web_root
+///        on PR branch received: copy bench_ci_web_root to gh-pages/repo_name/branch_name
+///     
+/// --file-name
+///     - write output to specified filename
 /// by default overwrites bench.cbor in cwd
 ///
 ///
@@ -52,5 +62,22 @@ fn main() {
             },
         ],
     );
-    results.save("name.cbor");
+    // TODO: OH I should just name with iso date lol
+    // TODO: handle unwraps
+    std::fs::create_dir_all("bench_ci_history").unwrap();
+    let max = std::fs::read_dir("bench_ci_history")
+        .unwrap()
+        .filter_map(|dir_entry| {
+            let name = dir_entry.unwrap().file_name();
+            let name = name.to_str().unwrap();
+            let number_portion = name.strip_prefix("bench")?.strip_suffix(".cbor")?;
+            number_portion.parse().ok()
+        })
+        .max()
+        .unwrap_or(0);
+    results.save(&format!("bench_ci_history/bench{}.cbor", max + 1));
+
+    // TODO: huh, I wonder if it would be easier to just read, process and then write the bench_ci_history_combined.cbor file directly without storing all the intermediate files.
+    // Would be cheaper on disk space...
+    // I probably could just do that considering I have --ci flag
 }
